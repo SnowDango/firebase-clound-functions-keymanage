5bff04f3e5b84e1b0c78ab526257dffb62e72a90
YukiOchiai <xdi.grand.you.winner@gmail.com>
2020-09-23 00:57:39 +0900

    [fixed]　コードの整理

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 import admin from "firebase-admin";
-admin.initializeApp(functions.config().firebase);
+import * as Router from "./routes/Router";
 
-const routes = require('./routes/Router');
+admin.initializeApp(functions.config().firebase);
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const router: express.Router = express.Router();
-router.post('/user',(req, res) => {
+router.post('/user',(req:Express.Response, res:Express.Response) => {
+    const routes = new Router.default();
     routes.userRoutes(req,res);
 });
 
 router.post('/channel',(req,res) => {
+    const routes = new Router.default();
     routes.channelRoutes(req,res);
 });
 
--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -17,35 +17,40 @@ enum ChannelRouteCode {
     ADD_USER
 }
 
-exports.userRoutes = (req:any,res:any) => {
-    const body = req.body;
-    const routeCode:UserRouteCode = body.code;
-    const service = new UserService();
-    switch (routeCode) {
-        // TODO User操作のコードによる動作制御
-        case UserRouteCode.CREATE:
-            service.create(body,res).then().catch();
-            break;
-        case UserRouteCode.ADD_TOKEN:
-            service.addToken(body,res).then().catch();
-            break;
-        case UserRouteCode.REMOVE_TOKEN:
-            service.removeToken(body,res).then().catch();
-            break;
-        case UserRouteCode.UPDATE_TOKEN:
-            service.updateToken(body,res).then().catch();
-            break;
-        case UserRouteCode.CHANGE_NAME:
-            service.changeName(body,res).then().catch();
-            break;
-        case UserRouteCode.REMOVE_USER:
-            service.removeUser(body,res).then().catch();
-            break;
+export default class Router{
+
+    userRoutes = (req:any,res:any) => {
+        const body = req.body;
+        const routeCode:UserRouteCode = body.code;
+        const service = new UserService();
+        switch (routeCode) {
+            // TODO User操作のコードによる動作制御
+            case UserRouteCode.CREATE:
+                service.create(body,res).then().catch();
+                break;
+            case UserRouteCode.ADD_TOKEN:
+                service.addToken(body,res).then().catch();
+                break;
+            case UserRouteCode.REMOVE_TOKEN:
+                service.removeToken(body,res).then().catch();
+                break;
+            case UserRouteCode.UPDATE_TOKEN:
+                service.updateToken(body,res).then().catch();
+                break;
+            case UserRouteCode.CHANGE_NAME:
+                service.changeName(body,res).then().catch();
+                break;
+            case UserRouteCode.REMOVE_USER:
+                service.removeUser(body,res).then().catch();
+                break;
+        }
     }
-}
-exports.channelRoutes = (req:any,res:any) => {
-    const routesCode:ChannelRouteCode = req.body.code;
-    switch (routesCode) {
-        // TODO チャンネル操作のコードによる動作制御
+
+    channelRoutes = (req:any,res:any) => {
+        const routesCode:ChannelRouteCode = req.body.code;
+        switch (routesCode) {
+            // TODO チャンネル操作のコードによる動作制御
+        }
     }
+
 }
